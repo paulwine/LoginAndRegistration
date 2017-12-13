f8c00c28053379026ebe942539b235772c87c5c4
Paul <paulwinegard@gmail.com>
2017-12-13 15:09:41 -0800

    Url glitch fixed

--- main/main/settings.py
+++ main/main/settings.py
@@ -31,7 +31,7 @@ ALLOWED_HOSTS = []
 # Application definition
 
 INSTALLED_APPS = [
-    'apps.log_red'
+    'apps.log_reg',
     'django.contrib.admin',
     'django.contrib.auth',
     'django.contrib.contenttypes',
